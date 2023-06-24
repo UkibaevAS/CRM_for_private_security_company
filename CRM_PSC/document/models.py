ce0e0f10e03603c43ee5d402ffe9b5d169e17ef7
UkibaevAS <Ukibaev_AS@mail.ru>
2023-06-25 03:30:04 +0500

    Внесены изменения в модели

--- CRM_PSC/document/models.py
+++ CRM_PSC/document/models.py
@@ -13,12 +13,11 @@ class Document(models.Model):
         verbose_name_plural = _('Documents')
 
     name = models.CharField(max_length=30, db_index=True, verbose_name=_('name'))
-    series_and_number = models.CharField(max_length=30, null=False, blank=False, db_index=True, help_text=_("format data: dd.mm.yyyy"), verbose_name=_('series_and_number'))
+    series_and_number = models.CharField(max_length=30, null=False, blank=False, db_index=True, help_text=_("format: series number"), verbose_name=_('series_and_number'))
     date_issue = models.CharField(max_length=10, null=False, blank=False, db_index=True, help_text=_("format data: dd.mm.yyyy"), verbose_name=_('date_issue'))
     date_expiration = models.CharField(max_length=10, null=False, blank=False, db_index=True, help_text=_("format data: dd.mm.yyyy"), verbose_name=_('date_expiration'))
     who_issued = models.CharField(max_length=50, null=False, blank=False, db_index=True, verbose_name=_('who_issued'))
     place_registration = models.CharField(max_length=150, null=False, blank=False, db_index=True, verbose_name=_('place_registration'))
-    place_of_residence = models.CharField(max_length=150, null=False, blank=False, db_index=True, verbose_name=_('place_of_residence'))
     driving_license_categories = models.CharField(max_length=150, null=False, blank=True, db_index=True, verbose_name=_('driving_license_categories'))
     document_copy = models.ImageField(null=True, blank=True, upload_to=document_directory_path, verbose_name=_('document_copy'))
 
