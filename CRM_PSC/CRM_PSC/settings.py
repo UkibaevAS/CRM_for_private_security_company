ba02ff4b4a9be71928a8aa538eacdb1869da846d
UkibaevAS <Ukibaev_AS@mail.ru>
2023-07-02 15:51:08 +0500

    Версия 3.1.0 Локализация

--- CRM_PSC/CRM_PSC/settings.py
+++ CRM_PSC/CRM_PSC/settings.py
@@ -118,6 +118,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+USE_L10N = True
+
+LOCAL_PATHS = [
+    BASE_DIR / "locale/"
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CRM_for_private_security_company
 
+Требуется установка gettext 0.21 и выше
