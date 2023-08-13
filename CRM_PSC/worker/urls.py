e9281dfc74480d6ad77a39f88f4cf079e057ccb1
UkibaevAS <Ukibaev_AS@mail.ru>
2023-08-13 14:51:35 +0500

    Версия 6.2.0 для API сотрудника

--- CRM_PSC/worker/urls.py
+++ CRM_PSC/worker/urls.py
@@ -2,16 +2,23 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import WorkerViewSet
+from .views import WorkerViewSet, WorkerWithCategoryViewSet, WorkerWithoutCategoryViewSet
 
 
 app_name = "worker"
 
-routers = DefaultRouter()
-routers.register('worker', WorkerViewSet)
+router1 = DefaultRouter()
+router1.register('worker_view', WorkerViewSet)
 
+router2 = DefaultRouter()
+router2.register('category', WorkerWithCategoryViewSet)
 
+router3 = DefaultRouter()
+router3.register('_category', WorkerWithoutCategoryViewSet)
 
 urlpatterns = [
-    path('api/', include(routers.urls)),
+    path('', include(router1.urls)),
+    path('with_', include(router2.urls)),
+    path('without', include(router3.urls)),
 ]
+
--- CRM_PSC/worker/views.py
+++ CRM_PSC/worker/views.py
@@ -12,3 +12,17 @@ class WorkerViewSet(ModelViewSet):
         SearchFilter,
     ]
     search_fields = ['second_name']
+
+
+class WorkerWithCategoryViewSet(ModelViewSet):
+    queryset = Worker.objects.exclude(category='Нет разряда')
+    serializer_class = WorkerSerializer
+
+    # @action(methods=['get'], detail=False)
+    # def category(self, request):
+    #     cats = Category.objects.all()
+    #     return Response({'cats': [c.name for c in cats]})
+
+class WorkerWithoutCategoryViewSet(ModelViewSet):
+    queryset = Worker.objects.filter(category='Нет разряда')
+    serializer_class = WorkerSerializer
