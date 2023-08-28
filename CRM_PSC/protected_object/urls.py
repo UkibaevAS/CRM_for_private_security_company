b2889c9da7b6191a62469150f8eefc166ca0939b
UkibaevAS <Ukibaev_AS@mail.ru>
2023-08-28 16:07:54 +0500

    Версия 6.17.1

--- CRM_PSC/protected_object/urls.py
+++ CRM_PSC/protected_object/urls.py
@@ -12,8 +12,8 @@ app_name = 'protected_object'
 
 
 urlpatterns = [
+    path('', Protected_objectListView.as_view(), name='protected_objects'),
     path('client/', ClientListView.as_view(), name='clients'),
     path('performer/', PerformerListView.as_view(), name='performers'),
     path('post/', PostListView.as_view(), name='posts'),
-    path('protected_object/', Protected_objectListView.as_view(), name='protected_objects'),
 ]
